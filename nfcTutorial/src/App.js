f03000aedfdabcc9b983cde70c0bd7b126b069cd
sevimozsoy <sevimozsoy30@gmail.com>
2022-04-05 14:01:52 +0300

    This version checks if the device have nfc or not

---
+++ nfcTutorial/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, StyleSheet } from "react-native";
+import nfcManager from 'react-native-nfc-manager';
+
+function App() {
+    const [hasNfc, setHasNfc] = React.useState(null);
+
+    React.useEffect(() => {
+        async function checkNfc(){
+            setHasNfc(await nfcManager.isSupported());
+        }
+
+        checkNfc();
+    }, []);
+
+
+    if (hasNfc === null) {
+        return null;
+    } else if (!hasNfc) {
+        return (
+            <View style={styles.wrapper}>
+                <Text>Cihazınız NFC desteklemiyor.</Text>
+            </View>
+        )
+    }
+    return (
+        <View style={styles.wrapper}>
+            <Text>Hello Nfc</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default App;
